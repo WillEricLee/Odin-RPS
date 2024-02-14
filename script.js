1884c7b97f02eee8f32cda99517cb98b3f807ca0
Will Lee <willericlee@gmail.com>
2024-02-14 01:49:54 -0700

    Create first draft of RPS player func in js
    Created first draft tree of RPS logic. Takes two arguments for player
    choice and computer choice.
    
    Then, a tree of switch statements determines if the player wins or if
    it's a tie. Then, a display message is shown depending on what the
    calculated outcome is.

--- script.js
+++ script.js
@@ -15,3 +15,62 @@ function getComputerChoice() {
 
 	return 'wawa';
 }
+
+function playRound(playerSelection, computerSelection) {
+	let winner = false;
+	let tie = false;
+	switch(playerSelection) {
+		case 'Rock':
+			switch(computerSelection) {
+				case 'Rock':
+					tie = true;
+					break;
+				case 'Paper':
+					winner = false;
+					break;
+				case 'Scissors':
+					winner = true;
+					break;
+			}
+		break;
+
+		case 'Paper':
+			switch(computerSelection) {
+				case 'Rock':
+					winner = true;
+					break;
+				case 'Paper':
+					tie = true;
+				case 'Scissors':
+					winner = false;
+			}
+		break;
+
+		case 'Scissors':
+			switch(computerSelection) {
+				case 'Rock':
+					winner = false;
+					break;
+				case 'Paper':
+					winner = true;
+					break;
+				case 'Scissors':
+					tie = true;
+					break;
+			}
+		break;
+
+	
+	}
+
+	if (tie) {
+		return "Tie, you both showed " + playerSelection;
+	}
+	else if (winner) {
+		return "You won, your " + playerSelection + " beats " + computerSelection;
+	}
+	else {
+		return "You lost, your " + playerSelection + " loses to " + computerSelection;
+	}
+	return 'wawa';	
+}
